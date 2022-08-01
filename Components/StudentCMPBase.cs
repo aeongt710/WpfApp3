1b496966f2ed8e1e4125676dcd368b2185f42166
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-08-01 15:45:55 +0500

    Add project files.

---
+++ Components/StudentCMPBase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.Models;
+
+namespace WpfApp3.Components
+{
+    public class StudentCMPBase : ComponentBase
+    {
+        [Parameter]
+        public List<string> ListOfString { get; set; }
+        //[ParameterAttribute]
+        //public string? ChildData { get; set; } 
+        [Parameter] public EventCallback<StudentModel> EventCallBackProperty { get; set; }
+        public StudentModel StudentModelObj { get; set; } = new StudentModel();
+
+        //[Inject]
+        //public IMapper Mapper { get; set; }
+
+
+        protected async Task HandleValidSubmit()
+        {
+            var obj = StudentModelObj;
+            await EventCallBackProperty.InvokeAsync(StudentModelObj);
+        }
+    }
+
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Age { get; set; }
+        public string? Description { get; set; }
+    }
+}
